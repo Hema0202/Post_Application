18f144fdb0ba83f327ca04d98e7cabf973ebdfc2
Hema0202 <hemalohana8@gmail.com>
2024-05-27 21:33:53 +0530

    signup and login controller added

---
+++ Api/routes/controller/signupController.js
@@ -0,0 +1,61 @@
+const userModel = require('./../model/userModel');
+
+async function signupController(req,res){
+    try {
+       
+        let data = req.body;
+        console.log(data);
+
+        let {name, email, password} = data;
+
+        if(!name){
+            return res.send({
+                status:false,
+                message: 'name is required'
+            })
+        }
+        if(!email){
+            return res.send({
+                status:false,
+                message: 'email is required'
+            })
+        }
+        if(!password){
+            return res.send({
+                status:false,
+                message: 'password is required'
+            })
+        }
+
+        //handle unique email
+        const oldUser = await userModel.findOne({
+            email: email,
+        });
+
+        if(oldUser){
+            return res.send({
+                status: false,
+                message: `${email} is already registered`,
+            });
+        }
+
+        const user = await userModel.create({
+            name: name,
+            email: email,
+            password: password,
+        });
+        res.send({
+            status: true,
+            data: user,
+            message: "You are registered successfully",
+        });
+
+    } catch(err) {
+        res.send({
+            status: false,
+            message: err.message,
+        });
+    }
+}
+
+module.exports = signupController;
--- Api/routes/route.js
+++ Api/routes/route.js
@@ -6,18 +6,29 @@ const{
     updateUser,
     deleteUser
 } = require('./controller/userController');
+const signupController = require('./controller/signupController');
+const loginController = require('./controller/loginController');
+const auth = require('./middlewares/auth')
 
-router.get('/test',(req,res) => res.send('Working fine'));
+function testHandler(req,res){
+    res.send('everything working fine');
+}
+
+router.get('/test',auth,testHandler);
+
+router.post('/signup',signupController);
+
+router.post('/login', loginController);
 
 router.get('api/user/create',createUser);
 
-router.get('api/user/get/:id', getUserById);
+router.get('api/user/get/:id',auth, getUserById);
 
 router.get('api/user/getAll',getUsers);
 
-router.put('api/user/update/:id',updateUser);
+router.put('api/user/update/:id',auth ,updateUser);
 
-router.delete('api/user/delete/:id', deleteUser);
+router.delete('api/user/delete/:id',auth, deleteUser);
 
 router.all('/*',(req,res)=>res.status(404).send({
     status: false,
